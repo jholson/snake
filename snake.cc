bfbc64a83e4f8bd97a571483fe1029392cee578a
Robert Liu <robqliu@gmail.com>
2013-10-01 02:10:24 -0700

    refactored to use Game_State, but segfaults on generating food

--- snake.cc
+++ snake.cc
@@ -41,6 +41,7 @@ struct GameState
 {
 	std::deque<SnakeNode> snake;
 	std::vector<std::vector<BoardCellType> > board;
+	Direction curr_direction;
 	int food_row, food_col;
 };
 
@@ -50,12 +51,11 @@ GameStatus g_status;
 
 int menu_mode();
 void in_game_mode();
-int in_game_core(std::deque<SnakeNode> &snake, std::vector<std::vector<BoardCellType> > &board,
-	Direction &curr_direction);
-int process_input(int inp, std::deque<SnakeNode> &snake,
-	std::vector<std::vector<BoardCellType> > &board, Direction &curr_direction);
+int in_game_core(GameState &game_state);
+int process_input(int inp, GameState &game_state);
 long diff_in_nanoseconds(timespec first, timespec second);
-void print_board(std::deque<SnakeNode> &snake);
+void generate_food(GameState &game_state);
+void print_game(GameState &game_state);
 void configure_stdscr();
 void configure_colors();
 
@@ -140,33 +140,39 @@ int menu_mode()
 void in_game_mode()
 {
 	// Init game settings
-	// TODO: Wrap this in a game state struct
-	Direction curr_direction = Up;
-	// We need an extra row/col because of how we deal with board edge detection
-	std::vector<std::vector<BoardCellType> > board(
-		g_max_row + 1, std::vector<BoardCellType>(g_max_col + 1, Empty));
-	std::deque<SnakeNode> snake;
-	snake.push_front(SnakeNode(g_max_col/2, g_max_row/2));
-	// TODO: Temporarily hard-coded for testing
-	int food_col = 30, food_row = 30;
+	GameState game_state =
+	{
+		std::deque<SnakeNode>(), // snake
+		std::vector<std::vector<BoardCellType> >(
+			g_max_row,
+			std::vector<BoardCellType>(g_max_col, Empty)), // board
+		Up, // curr_direction
+		// TODO: Temporarily hard-coded for testing
+		30, // food_col
+		30 // food_row
+	};
+	// Start the snake of size 1 in the middle of the board
+	game_state.snake.push_front(SnakeNode(g_max_col/2, g_max_row/2));
+
 	erase();
 	attrset(A_BOLD);
 
 	// This uses the same style as the ncurses window so (x, y) <-> [x][y] <-> (col, row)
-	board[food_col][food_row] = Food;
-	board[snake.front().col][snake.front().row] = Snake;
+	game_state.board[game_state.food_col][game_state.food_row] = Food;
+	game_state.board[game_state.snake.front().col][game_state.snake.front().row] = Snake;
 
 	while (true)
 	{
-		if (in_game_core(snake, food_col, food_row, board, curr_direction) < 0)
+		if (in_game_core(game_state) < 0)
 		{
 			// Game ended
 			return;
 		}
 
 		// Print some diagnostics
-		mvprintw(0, 0, "col: %d, row: %d\n", snake.front().col, snake.front().row);
-		mvprintw(1, 0, "curr_direction: %d\n", curr_direction);
+		mvprintw(0, 0, "col: %d, row: %d\n", game_state.snake.front().col, 
+			game_state.snake.front().row);
+		mvprintw(1, 0, "curr_direction: %d\n", game_state.curr_direction);
 
 		refresh();
 
@@ -175,15 +181,11 @@ void in_game_mode()
 	}
 }
 
-int in_game_core(std::deque<SnakeNode> &snake, std::vector<std::vector<BoardCellType> > &board, 
-	Direction &curr_direction)
+int in_game_core(GameState &game_state)
 {
 	const int NANOSECONDS_PER_MILLISECOND = 1000000;
 	// In nanoseconds
 	long refresh_duration = 100 * NANOSECONDS_PER_MILLISECOND;
-
-	// Process snake before the "round" starts (basically, check for collision with food)
-	bool extend_snake = update_food(snake, board);
 
 	// Next the "round" occurs which consists of reading user input until refresh_duration has
 	// elapsed
@@ -226,13 +228,13 @@ int in_game_core(std::deque<SnakeNode> &snake, std::vector<std::vector<BoardCell
 	nodelay(stdscr, false);
 
 	// Now we're done with the main portion of "round", process the results of it
-	if (process_input(last_dir_inp, snake, board, curr_direction) < 0)
+	if (process_input(last_dir_inp, game_state) < 0)
 	{
 		return -1;
 	}
 
-	// Finally update the board
-	print_board(snake);
+	// Finally draw the game
+	print_game(game_state);
 
 	return 0;
 }
@@ -245,40 +247,38 @@ long diff_in_nanoseconds(timespec first, timespec second)
 		(second.tv_nsec - first.tv_nsec);
 }
 
-bool update_food(std::deque<SnakeNode> &snake, std::vector
-
-int process_input(int inp, std::deque<SnakeNode> &snake,
-	std::vector<std::vector<BoardCellType> > &board, Direction &curr_direction)
+bool update_food(GameState &game_state)
 {
-	// Update curr_direction
+	// TODO:
+	return true;
+}
+
+int process_input(int inp, GameState &game_state)
+{
+	// Map input to a direction
 	switch (inp)
 	{
 		case KEY_DOWN:
-			curr_direction = Down;
+			game_state.curr_direction = Down;
 			break;
 		case KEY_UP:
-			curr_direction = Up;
+			game_state.curr_direction = Up;
 			break;
 		case KEY_LEFT:
-			curr_direction = Left;
+			game_state.curr_direction = Left;
 			break;
 		case KEY_RIGHT:
-			curr_direction = Right;
+			game_state.curr_direction = Right;
 			break;
 		default:
 			// No input was entered so the curr_direction stays the same
 			break;
 	}
 
-	// Update snake
-	// Tail first
-	board[snake.back().col][snake.front().row] = Empty;
-	snake.pop_back();
-
-	// Now the head
-	int curr_col = snake.front().col;
-	int curr_row = snake.front().row;
-	switch (curr_direction)
+	// Move the snake
+	int curr_col = game_state.snake.front().col;
+	int curr_row = game_state.snake.front().row;
+	switch (game_state.curr_direction)
 	{
 		case Down:
 			curr_row++;
@@ -300,28 +300,63 @@ int process_input(int inp, std::deque<SnakeNode> &snake,
 	// Note the short circuit logic
 	if ((curr_row < 0 || curr_row > g_max_row) ||
 	    (curr_col < 0 || curr_col > g_max_col) ||
-	    (board[curr_row][curr_col] == Snake))
+	    // Short circuit is critical here due to index out of bounds
+	    (game_state.board[curr_row][curr_col] == Snake))
 	{
 		// They lost
 		return -1;
 	}
-	snake.push_front(SnakeNode(curr_col, curr_row));
-	// Update the board
-	board[snake.front().col][snake.front().row] = Snake;
+	game_state.snake.push_front(SnakeNode(curr_col, curr_row));
+	game_state.board[game_state.snake.front().col][game_state.snake.front().row] = Snake;
 
+	// Update the tail based on whether the snake ate food
+	if (game_state.snake.front().col == game_state.food_col &&
+		game_state.snake.front().row == game_state.food_row)
+	{
+		// Then we ate food, so we "grow" the snake by... not making it shorter
+		
+		// Now generate new food
+		generate_food(game_state);
+	}
+	else
+	{
+		game_state.board[game_state.snake.back().col][game_state.snake.back().row] = Empty;
+		game_state.snake.pop_back();
+	}
+	
 	return 0;
 }
 
-void print_board(std::deque<SnakeNode> &snake)
+void generate_food(GameState &game_state)
+{
+	// TODO: Since the board is small we can actually enumerate the entire thing. Since the
+	// snake could theoretically cover (almost) the entire board it doesn't seem asymptotically
+	// possible to improve the time. It could be feasible to store the empty spots in a list
+	// or perhaps store the number of empty cells in the rectangle from (0,0) to a coordinate
+	// (x,y) and then use some sort of binary decision tree to pick an arbitrary empty spot
+	std::vector<std::pair<int, int> > empty_cells;
+	for (int col = 0; col < g_max_col; col++)
+	{
+		for (int row = 0; row < g_max_row; row++)
+		{
+			if (game_state.board[col][row] == Empty)
+			{
+				empty_cells.push_back(std::pair<int, int>(col, row));
+			}
+		}
+	}
+}
+
+void print_game(GameState &game_state)
 {
 	erase();
-	for (std::deque<SnakeNode>::iterator it = snake.begin(); it != snake.end(); it++)
+	for (std::deque<SnakeNode>::iterator it = game_state.snake.begin();
+		it != game_state.snake.end(); it++)
 	{
 		mvaddch((*it).row, (*it).col, ACS_BLOCK);
 	}
 
-	// TODO: Testing out food
-	mvaddch(30, 30, ACS_PI);
+	mvaddch(game_state.food_col, game_state.food_row, ACS_PI);
 }
 
 void configure_stdscr()
